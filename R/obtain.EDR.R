da66ac52eb5e5075a1fb56fcba9fa9e00f4ab0e6
liupeng2117 <liupeng2117@gmail.com>
2017-10-15 15:12:17 -0400

    Add files via upload

---
+++ R/obtain.EDR.R
@@ -0,0 +1,17 @@
+#' Title Obtain predicted EDR
+#'
+#' @param result input result from the function Estimate.EDR.from.pilot 
+#'
+#' @return a list of results, including EDR, FDR etc
+#' @export obtain.EDR
+#'
+#' @examples 
+#' 
+#' EDR<-obtain.EDR(result=power.result)
+#' 
+#' 
+obtain.EDR<-function(result){
+  y<-result[[1]] #return edr
+  data.ref<-Reduce("+",y)/length(y)
+  return(list(data.ref,y))
+}
