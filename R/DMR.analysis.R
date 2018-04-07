eed937e4dbcb45475c6daac20a6f6bd468a832e9
liupeng2117 <liupeng2117@gmail.com>
2018-04-06 23:14:08 -0400

    Add files via upload

--- R/DMR.analysis.R
+++ R/DMR.analysis.R
@@ -10,102 +10,103 @@
 #' @export DMR.analysis
 #'
 #' @examples
-DMR.analysis<-function(x,cov.matrix,methyl.matrix){
-  fai.est<-estimate.fai2(x=x, cov.matrix, methyl.matrix)
-  test.result<-Z.wald(x, cov.matrix, methyl.matrix, fai=fai.est,a=1)
-  p.values<-test.result[[1]]
-  model<-cbind(beta0=test.result[[1]],beta1=test.result[[2]],fai=fai.est)
-  return(list(p.values,model))
+DMR.analysis <- function(x, cov.matrix, methyl.matrix) {
+    fai.est <- estimate.fai2(x = x, cov.matrix, methyl.matrix)
+    test.result <- Z.wald(x, cov.matrix, methyl.matrix, fai = fai.est, a = 1)
+    p.values <- test.result[[1]]
+    model <- cbind(beta0 = test.result[[1]], beta1 = test.result[[2]], fai = fai.est)
+    return(list(p.values, model))
 }
 
 
 # Z-value + wald-test
-Z.wald<-function(x,cov.matrix,sim.meth,fai,a=1){
-  G<-dim(cov.matrix)[1]
-  D<-sum(x)
-  n.groups<-length(x)
-  # Assign each group size
-  
-  for(i in 1:n.groups){
-    assign(paste0("D",i),x[i])
-  }
-  
-  ### calculate beta1-hat
-  chi2<-rep(0,G)
-  
-  for(i in 1:G){
-    tryCatch({
-      X<-matrix(c(rep(1,D),rep(1,D1),rep(0,D-D1)),ncol=2)
-      
-      if(n.groups>2){
-        c<-D1
-        for(j in 2:(n.groups-1)){
-          col<-c(rep(0,c),rep(1,get(paste0("D",j))),rep(0,D-c-get(paste0("D",j))))
-          X<-cbind(X,col)
-          c=c+get(paste0("D",j))
-        }        
-      }
-      
-      V.inverse<-diag(cov.matrix[i,]/(1+(cov.matrix[i,]-1)*fai))
-      Z<-asin(2*(sim.meth[i,]+a)/(cov.matrix[i,]+2*a)-1)
-      sigma<-solve(t(X)%*%V.inverse%*%X)
-      beta.hat<-sigma %*% t(X) %*% V.inverse %*% Z
-      
-      C<-diag(rep(1,n.groups))[,-1]
-      var<-t(C) %*% sigma %*% C
-      chi2[i]<-t(t(C) %*% beta.hat) %*% solve(var) %*% (t(C) %*% beta.hat)
-    }, error = function(err) {
-      # error handler picks up where error was generated
-      print(paste("MY_ERROR:  ",err))
-    })
-  }
-  # Calculate test statistic, p and q values
-  p.Zw<-pchisq(chi2,df=n.groups-1,lower.tail=F)
-  return(list(p.Zw=p.Zw,z.statistic=chi2))
+Z.wald <- function(x, cov.matrix, sim.meth, fai, a = 1) {
+    G <- dim(cov.matrix)[1]
+    D <- sum(x)
+    n.groups <- length(x)
+    # Assign each group size
+    
+    for (i in 1:n.groups) {
+        assign(paste0("D", i), x[i])
+    }
+    
+    ### calculate beta1-hat
+    chi2 <- rep(0, G)
+    
+    for (i in 1:G) {
+        tryCatch({
+            X <- matrix(c(rep(1, D), rep(1, D1), rep(0, D - D1)), ncol = 2)
+            
+            if (n.groups > 2) {
+                c <- D1
+                for (j in 2:(n.groups - 1)) {
+                  col <- c(rep(0, c), rep(1, get(paste0("D", j))), rep(0, D - c - get(paste0("D", j))))
+                  X <- cbind(X, col)
+                  c = c + get(paste0("D", j))
+                }
+            }
+            
+            V.inverse <- diag(cov.matrix[i, ]/(1 + (cov.matrix[i, ] - 1) * fai))
+            Z <- asin(2 * (sim.meth[i, ] + a)/(cov.matrix[i, ] + 2 * a) - 1)
+            sigma <- solve(t(X) %*% V.inverse %*% X)
+            beta.hat <- sigma %*% t(X) %*% V.inverse %*% Z
+            
+            C <- diag(rep(1, n.groups))[, -1]
+            var <- t(C) %*% sigma %*% C
+            chi2[i] <- t(t(C) %*% beta.hat) %*% solve(var) %*% (t(C) %*% beta.hat)
+        }, error = function(err) {
+            # error handler picks up where error was generated
+            print(paste("MY_ERROR:  ", err))
+        })
+    }
+    # Calculate test statistic, p and q values
+    p.Zw <- pchisq(chi2, df = n.groups - 1, lower.tail = F)
+    return(list(p.Zw = p.Zw, z.statistic = chi2))
 }
 
 
 
 ## Estimate fai using park's method
-estimate.fai2<-function(cov.matrix,methyl.matrix,a=1,x){
-  G<-dim(cov.matrix)[1]
-  D<-dim(cov.matrix)[2]
-  n.groups<-length(x)
-  # Assign each group size
-  for(i in 1:n.groups){
-    assign(paste0("D",i),x[i])
-  }
-  
-  if(D==2) fai.est=0.001 else{
-    fai<-rep(0,G)
-    for (i in 1:G){
-      tryCatch({
-        V0.inverse<-diag(cov.matrix[i,])
-        # X matrix
-        X<-matrix(c(rep(1,D),rep(1,D1),rep(0,D-D1)),ncol=2)
-        if(n.groups>2){
-          c<-D1
-          for(j in 2:(n.groups-1)){
-            col<-c(rep(0,c),rep(1,get(paste0("D",j))),rep(0,D-c-get(paste0("D",j))))
-            X<-cbind(X,col)
-            c=c+get(paste0("D",j))
-          }
-        }
-        Z<-asin(2*(methyl.matrix[i,]+a)/(cov.matrix[i,]+2*a)-1)
-        sigma<-solve(t(X)%*%V0.inverse%*%X)
-        beta.hat0<-sigma%*%t(X)%*%V0.inverse%*%Z
-        chi2<-sum(cov.matrix[i,]*(Z-X %*% beta.hat0)^2)
-        sigma2.hat<-chi2/(D-n.groups)
-        fai[i]<-(D*(sigma2.hat-1))/sum(cov.matrix[i,]-1)
-      }, error = function(err) {
-        # error handler picks up where error was generated
-        print(paste("MY_ERROR:  ",err))
-      })
+estimate.fai2 <- function(cov.matrix, methyl.matrix, a = 1, x) {
+    G <- dim(cov.matrix)[1]
+    D <- dim(cov.matrix)[2]
+    n.groups <- length(x)
+    # Assign each group size
+    for (i in 1:n.groups) {
+        assign(paste0("D", i), x[i])
     }
-    fai<-fai[complete.cases(fai)]
-    fai<-ifelse(fai<0,10^(-6),fai)
-    fai<-ifelse(fai>1,1-10^(-6),fai)
-    fai.est<-mean(fai)
-  }
-  return(fai.est)
+    
+    if (D == 2) 
+        fai.est = 0.001 else {
+        fai <- rep(0, G)
+        for (i in 1:G) {
+            tryCatch({
+                V0.inverse <- diag(cov.matrix[i, ])
+                # X matrix
+                X <- matrix(c(rep(1, D), rep(1, D1), rep(0, D - D1)), ncol = 2)
+                if (n.groups > 2) {
+                  c <- D1
+                  for (j in 2:(n.groups - 1)) {
+                    col <- c(rep(0, c), rep(1, get(paste0("D", j))), rep(0, D - c - get(paste0("D", j))))
+                    X <- cbind(X, col)
+                    c = c + get(paste0("D", j))
+                  }
+                }
+                Z <- asin(2 * (methyl.matrix[i, ] + a)/(cov.matrix[i, ] + 2 * a) - 1)
+                sigma <- solve(t(X) %*% V0.inverse %*% X)
+                beta.hat0 <- sigma %*% t(X) %*% V0.inverse %*% Z
+                chi2 <- sum(cov.matrix[i, ] * (Z - X %*% beta.hat0)^2)
+                sigma2.hat <- chi2/(D - n.groups)
+                fai[i] <- (D * (sigma2.hat - 1))/sum(cov.matrix[i, ] - 1)
+            }, error = function(err) {
+                # error handler picks up where error was generated
+                print(paste("MY_ERROR:  ", err))
+            })
+        }
+        fai <- fai[complete.cases(fai)]
+        fai <- ifelse(fai < 0, 10^(-6), fai)
+        fai <- ifelse(fai > 1, 1 - 10^(-6), fai)
+        fai.est <- mean(fai)
+    }
+    return(fai.est)
 }
--- R/Estimate.EDR.from.pilot.R
+++ R/Estimate.EDR.from.pilot.R
@@ -7,239 +7,226 @@
 #' @param M Number of parametric bootstrap, default is 10
 #' @param N0 pilot sample size per group
 #' @param target.N target sample size per group
-#' @param method by defualt "CBUM+CDD"
-#' @param transform.null equals False, we do not transform test statistics for non-DMR genes
+#' @param method by defualt 'CBUM+CDD'
 #'
 #' @return a list of estimation result
 #' @export Estimate.EDR.from.pilot
 #'
 #' @examples
-Estimate.EDR.from.pilot <- function(p.values, model,thresh.p=0.005,
-                                    FDR=0.05, M=10,
-                                    N0, target.N,
-                                    method="CBUM+CDD",transform.null = F){
-  
-  q.value = p.adjust(p.values, method = "BH") ##?
-  
-  Fitted.Model = switch(method, 
-                        "CBUM+CDD" = MixtureModel.Fittting.pi02(p.values,thresh.p=thresh.p,restrict = F)
-                        )
-  
-  p.values.mod <- p.values
-  if (any(is.na(p.values.mod))) {
-    p.values.mod <- p.values.mod[!is.na(p.values.mod)]
-    model = model[!is.na(p.values.mod),]
-  }
-  if (min(p.values.mod) == 0) {
-    min.nonzero <- min(p.values.mod[p.values.mod > 0])
-    p.values.mod[p.values.mod == 0] <- min.nonzero/2
-  }
-  if (max(p.values.mod) == 1) {
-    max.nonone <- max(p.values.mod[p.values.mod < 1])
-    p.values.mod[p.values.mod == 1] <- 1-(1-max.nonone)/2
-  }
-  
-  ngenes = length(p.values.mod)
-  
-  if(Fitted.Model[1]=="error"){
-    return("error")
-  }else{
-    lambda = as.numeric(Fitted.Model[1])
-    r=as.numeric(Fitted.Model[2])
-    s=as.numeric(Fitted.Model[3])
-    if(method == "TwoBeta"){
-      r2=as.numeric(Fitted.Model[4])
-      s2=as.numeric(Fitted.Model[5])
-      posterior = (lambda*dbeta(p.values.mod,r2,s2))/(dbeta(p.values.mod,r,s)*(1-lambda)+lambda*dbeta(p.values.mod,r2,s2)) #prob be non-DE
-    } else posterior = lambda/(dbeta(p.values.mod,r,s)*(1-lambda)+lambda) #prob be non-DE
+Estimate.EDR.from.pilot <- function(p.values, model, N0, target.N, thresh.p = 0.005, FDR = 0.05, M = 10, method = "CBUM+CDD") {
     
-    if(lambda>0.99|lambda<0.5){
-      #return(Fitted.Model)
-      print(paste("lambda >0.95 or <0.5,lambda=",Fitted.Model[1]))
+    Fitted.Model = switch(method, `CBUM+CDD` = MixtureModel.Fittting.pi02(p.values, thresh.p = thresh.p, restrict = F))
+    
+    p.values.mod <- p.values
+    if (any(is.na(p.values.mod))) {
+        p.values.mod <- p.values.mod[!is.na(p.values.mod)]
+        model = model[!is.na(p.values.mod), ]
     }
-    else{
-      parameter = list(n.old = N0, n.new= target.N)
-      Resampling <- function(target.N){
-        # DE_status_bootstrap = sample(c(FALSE,TRUE),ngenes,prob = c(lambda,1-lambda),replace=TRUE)
-        DE_status_posterior = sapply(1:length(posterior),function(x) sample(c(FALSE,TRUE),1,prob = c(posterior[x],1-posterior[x]),replace=TRUE))
-        
-        transform.p.values.norm <- function(p.values.each, DE_status_each, parameter, #model, 
-                                           transform.null = F){
-          n.old = parameter[[1]]
-          n.new = parameter[[2]]
-          
-          if(DE_status_each){
-            #beta.estimate = model[1:2]
-            #delta = model[3]
-            statistic.old = qnorm(p.values.each/2,lower.tail=FALSE)
-            old<-n.old
-            new<-n.new
-            
-            statistic.new = statistic.old*sqrt(new/old)
-            
-            p.values.star.star = (1-pnorm(abs(statistic.new)))*2
-            return(p.values.star.star)
-          } else if(transform.null) return(rep(runif(1),length(n.new)))
-          else return(rep(p.values.each, length(n.new)))
-          # } else{return(rep(p.values.each,length(n.new)))}
-        }
-        
-        
-
-        # p.values.star.posterior.old = sapply(1:ngenes,function(x) transform.p.values.old(p.values.mod[x], DE_status_posterior[x], N0,
-        #                                                                            sample.size, transform.null = transform.null))
-        p.values.star.posterior = sapply(1:ngenes,function(x) 
-                                                         transform.p.values.norm(p.values.mod[x], 
-                                                         DE_status_posterior[x], 
-                                                         parameter = parameter,
-                                                         transform.null = transform.null))
-        
-        
-        
-        p.values.star.posterior = matrix(p.values.star.posterior, ncol = length(target.N), byrow = T)
-        
-        Estimate_Posterior <- function(p.values.star.star){
-          if (min(p.values.star.star,na.rm=T) == 0) {
-            min.nonzero <- min(p.values.star.star[p.values.star.star > 0])
-            p.values.star.star[p.values.star.star == 0] <- min.nonzero/2
-          }
-          if (max(p.values.star.star,na.rm=T) == 1) {
-            max.non1 <- max(p.values.star.star[p.values.star.star <1])
-            p.values.star.star[p.values.star.star == 1] <- (max.non1+1)/2
-          }
-          ## empirical FDR control
-          p.DE=p.values.star.star[DE_status_posterior]
-          p.nonDE=p.values.star.star[!DE_status_posterior]
-          p.unique=sort(unique(p.values.star.star))
-          
-          FDR.unique <- vector(length=length(p.unique))
-          for(i in 1:length(p.unique)){
-            FDR.unique[i]=sum(p.nonDE<=p.unique[i])/sum(p.values.star.star<=p.unique[i])
-          }
-          index = which(FDR.unique<=FDR)
-          p.values.cut = if(length(index)){
-            p.unique[max(index)]
-          } else NA #everything is beyond FDR 0.05
-          if(min(p.values.star.star)>p.values.cut | is.na(p.values.cut)){Declare_status=rep("nonDE",ngenes)}
-          else{
-            Declare_status = DE_status_posterior
-            Declare_status[which(p.values.star.star<=p.values.cut)] = "DE"
-            Declare_status[-which(p.values.star.star<=p.values.cut)] = "nonDE"
-          }
-          A = sum((Declare_status=="nonDE")*(!DE_status_posterior))
-          B = sum((Declare_status=="nonDE")*(DE_status_posterior))
-          C = sum((Declare_status=="DE")*(!DE_status_posterior))
-          D = sum((Declare_status=="DE")*(DE_status_posterior))
-          if((C+D)==0){
-            TP_hat_post=0
-            ## no declared genes
-          }
-          else{
-            TP_hat_post = D/(C+D)
-          }
-          if((A+B)==0){
-            TN_hat_post=0
-            ## no declared genes
-          }
-          else{
-            TN_hat_post = A/(A+B)
-          }
-          # if(mean(p.values)>0.5+1.96*sqrt(1/(12*length(p.values)))){EDR_post=D/length(p.values)}
-          # else{EDR_post = D/(B+D)}
-          EDR_post = D/(B+D)
-          Declare_post  = sum(Declare_status=="DE")
-          # return(c(TP_hat_post,TN_hat_post,EDR_post,Declare_post, B = B, D = D, p.values.cut = p.values.cut))
-          return(c(TP_hat_post, TN_hat_post, EDR_post, Declare_post, A = A,B = B, C = C, D = D,
-                   p.values.cut = p.values.cut, FDR = FDR.unique[max(index)]))
-        }
-        
-        Estimate.Posterior.Result = matrix(apply(p.values.star.posterior,2,Estimate_Posterior), ncol = length(target.N)); round(Estimate.Posterior.Result, 3)
-        
-        row.names(Estimate.Posterior.Result) = c("TP", "TN", "EDR", "DeclareDE", "A", "B", "C", "D", "FDRcut", "FDR")
-        colnames(Estimate.Posterior.Result) = paste(target.N[1], "x" ,"2")
-        return(Estimate.Posterior.Result)
-      }
-      Result = lapply(1:M,function(x){
-        #print(x)
-        Resampling(target.N)
-      })
-      
-      ave.result<-Reduce("+",Result)/length(Result)
-      pred<-list(EDR=ave.result["EDR",],DeclareDE=ave.result["DeclareDE",],FDR=ave.result["FDR",])
-      return(pred) 
+    if (min(p.values.mod) == 0) {
+        min.nonzero <- min(p.values.mod[p.values.mod > 0])
+        p.values.mod[p.values.mod == 0] <- min.nonzero/2
     }
-  }
+    if (max(p.values.mod) == 1) {
+        max.nonone <- max(p.values.mod[p.values.mod < 1])
+        p.values.mod[p.values.mod == 1] <- 1 - (1 - max.nonone)/2
+    }
+    
+    ngenes = length(p.values.mod)
+    
+    if (Fitted.Model[1] == "error") {
+        return("error")
+    } else {
+        lambda = as.numeric(Fitted.Model[1])
+        r = as.numeric(Fitted.Model[2])
+        s = as.numeric(Fitted.Model[3])
+        if (method == "TwoBeta") {
+            r2 = as.numeric(Fitted.Model[4])
+            s2 = as.numeric(Fitted.Model[5])
+            posterior = (lambda * dbeta(p.values.mod, r2, s2))/(dbeta(p.values.mod, r, s) * (1 - lambda) + lambda * dbeta(p.values.mod, 
+                r2, s2))  #prob be non-DE
+        } else posterior = lambda/(dbeta(p.values.mod, r, s) * (1 - lambda) + lambda)  #prob be non-DE
+        
+        if (lambda > 0.99 | lambda < 0.5) {
+            # return(Fitted.Model)
+            print(paste("lambda >0.95 or <0.5,lambda=", Fitted.Model[1]))
+        } else {
+            parameter = list(n.old = N0, n.new = target.N)
+            Resampling <- function(target.N) {
+                # DE_status_bootstrap = sample(c(FALSE,TRUE),ngenes,prob = c(lambda,1-lambda),replace=TRUE)
+                DE_status_posterior = sapply(1:length(posterior), function(x) sample(c(FALSE, TRUE), 1, prob = c(posterior[x], 
+                  1 - posterior[x]), replace = TRUE))
+                
+                transform.p.values.norm <- function(p.values.each, DE_status_each, parameter) {
+                  n.old = parameter[[1]]
+                  n.new = parameter[[2]]
+                  
+                  if (DE_status_each) {
+                    # beta.estimate = model[1:2] delta = model[3]
+                    statistic.old = qnorm(p.values.each/2, lower.tail = FALSE)
+                    old <- n.old
+                    new <- n.new
+                    
+                    statistic.new = statistic.old * sqrt(new/old)
+                    
+                    p.values.star.star = (1 - pnorm(abs(statistic.new))) * 2
+                    return(p.values.star.star)
+                  } else return(rep(p.values.each, length(n.new)))
+                  # } else{return(rep(p.values.each,length(n.new)))}
+                }
+                
+                p.values.star.posterior = sapply(1:ngenes, function(x) transform.p.values.norm(p.values.mod[x], DE_status_posterior[x], 
+                  parameter = parameter))
+                
+                
+                
+                p.values.star.posterior = matrix(p.values.star.posterior, ncol = length(target.N), byrow = T)
+                
+                Estimate_Posterior <- function(p.values.star.star) {
+                  if (min(p.values.star.star, na.rm = T) == 0) {
+                    min.nonzero <- min(p.values.star.star[p.values.star.star > 0])
+                    p.values.star.star[p.values.star.star == 0] <- min.nonzero/2
+                  }
+                  if (max(p.values.star.star, na.rm = T) == 1) {
+                    max.non1 <- max(p.values.star.star[p.values.star.star < 1])
+                    p.values.star.star[p.values.star.star == 1] <- (max.non1 + 1)/2
+                  }
+                  ## empirical FDR control
+                  p.DE = p.values.star.star[DE_status_posterior]
+                  p.nonDE = p.values.star.star[!DE_status_posterior]
+                  p.unique = sort(unique(p.values.star.star))
+                  
+                  FDR.unique <- vector(length = length(p.unique))
+                  for (i in 1:length(p.unique)) {
+                    FDR.unique[i] = sum(p.nonDE <= p.unique[i])/sum(p.values.star.star <= p.unique[i])
+                  }
+                  index = which(FDR.unique <= FDR)
+                  p.values.cut = if (length(index)) {
+                    p.unique[max(index)]
+                  } else NA  #everything is beyond FDR 0.05
+                  if (min(p.values.star.star) > p.values.cut | is.na(p.values.cut)) {
+                    Declare_status = rep("nonDMR", ngenes)
+                  } else {
+                    Declare_status = DE_status_posterior
+                    Declare_status[which(p.values.star.star <= p.values.cut)] = "DMR"
+                    Declare_status[-which(p.values.star.star <= p.values.cut)] = "nonDMR"
+                  }
+                  A = sum((Declare_status == "nonDMR") * (!DE_status_posterior))
+                  B = sum((Declare_status == "nonDMR") * (DE_status_posterior))
+                  C = sum((Declare_status == "DMR") * (!DE_status_posterior))
+                  D = sum((Declare_status == "DMR") * (DE_status_posterior))
+                  if ((C + D) == 0) {
+                    TP_hat_post = 0
+                    ## no declared genes
+                  } else {
+                    TP_hat_post = D/(C + D)
+                  }
+                  if ((A + B) == 0) {
+                    TN_hat_post = 0
+                    ## no declared genes
+                  } else {
+                    TN_hat_post = A/(A + B)
+                  }
+                  # if(mean(p.values)>0.5+1.96*sqrt(1/(12*length(p.values)))){EDR_post=D/length(p.values)} else{EDR_post = D/(B+D)}
+                  EDR_post = D/(B + D)
+                  Declare_post = sum(Declare_status == "DMR")
+                  # return(c(TP_hat_post,TN_hat_post,EDR_post,Declare_post, B = B, D = D, p.values.cut = p.values.cut))
+                  return(c(TP_hat_post, TN_hat_post, EDR_post, Declare_post, A = A, B = B, C = C, D = D, p.values.cut = p.values.cut, 
+                    FDR = FDR.unique[max(index)]))
+                }
+                
+                Estimate.Posterior.Result = matrix(apply(p.values.star.posterior, 2, Estimate_Posterior), ncol = length(target.N))
+                round(Estimate.Posterior.Result, 3)
+                
+                row.names(Estimate.Posterior.Result) = c("TP", "TN", "EDR", "DeclareDMR", "A", "B", "C", "D", "FDRcut", 
+                  "FDR")
+                colnames(Estimate.Posterior.Result) = paste(target.N[1], "x", "2")
+                return(Estimate.Posterior.Result)
+            }
+            Result = lapply(1:M, function(x) {
+                # print(x)
+                Resampling(target.N)
+            })
+            
+            ave.result <- Reduce("+", Result)/length(Result)
+            pred <- list(EDR = ave.result["EDR", ], DeclareDMR = ave.result["DeclareDMR", ], FDR = ave.result["FDR", ])
+            return(pred)
+        }
+    }
 }
 
 
 
 # Use censored beta-uniform mixer model to estimate pi0(lambda)
-MixtureModel.Fittting.pi02 <- function(p.values, restrict = TRUE, l.upper = 0.95,thresh.p=0.05){
-  library(limma)
-  if (all(is.na(p.values))) {
-    stop("all p-values were NA; nothing to compute")
-  }
-  orig.pvals <- p.values
-  if (any(is.na(p.values))) {
-    p.values <- p.values[!is.na(pvals)]
-  }
-  if (min(p.values) == 0) {
-    min.nonzero <- min(p.values[p.values > 0])
-    p.values[p.values == 0] <- min.nonzero/2
-  }
-  
-  fBUM <- function(z,d) {
-    p.values = d
-    r = z[1]
-    s = z[2]
-    Target.Func = -sum(log(lambda+(1-lambda)*p.values^(r-1)*(1-p.values)^(s-1)/beta(r,s)))
-    return(Target.Func)
-  }
-  
-  ## inital value(MME and non-parametric)
-  mean.p = mean(p.values)
-  var.p = var(p.values)
-  init.r = ((1-mean.p)*mean.p^2-mean.p*var.p)/var.p
-  init.s= ((1-mean.p)^2*mean.p-(1-mean.p)*var.p)/var.p
-  init = c(max(0,min(init.r,.9)),max(1,init.s))
-  
-  if(restrict){
-    lambda = max(min(CBUM(p.values,thresh.censor=thresh.p),l.upper), 0.8)
-    r.upper = .9
-    s.upper = Inf
-    r.lower = 0
-    s.lower = 1
-  } else{
-    lambda = min(CBUM(p.values,thresh.censor=thresh.p),0.99)
-    r.upper = 1
-    s.upper = Inf
-    r.lower = 0
-    s.lower = 1
-  }
-  if(unique(tryCatch(print(c(optim(init, fBUM,d=p.values,method= "L-BFGS-B",upper=c(r.upper,s.upper),lower=c(r.lower,s.lower))$par)),error = function(e) {print("error")})=="error")){return(init)}
-  else{
-    pars = optim(init, fBUM,d=p.values,method= "L-BFGS-B",upper=c(r.upper,s.upper),lower=c(r.lower,s.lower))$par
-    LL=sum(log(lambda+(1-lambda)*p.values^(pars[1]-1)*(1-p.values)^(pars[2]-1)/beta(pars[1],pars[2])))
-    return(c(lambda,pars,LL))
-  }
+MixtureModel.Fittting.pi02 <- function(p.values, restrict = TRUE, l.upper = 0.95, thresh.p = 0.05) {
+    library(limma)
+    if (all(is.na(p.values))) {
+        stop("all p-values were NA; nothing to compute")
+    }
+    orig.pvals <- p.values
+    if (any(is.na(p.values))) {
+        p.values <- p.values[!is.na(pvals)]
+    }
+    if (min(p.values) == 0) {
+        min.nonzero <- min(p.values[p.values > 0])
+        p.values[p.values == 0] <- min.nonzero/2
+    }
+    
+    fBUM <- function(z, d) {
+        p.values = d
+        r = z[1]
+        s = z[2]
+        Target.Func = -sum(log(lambda + (1 - lambda) * p.values^(r - 1) * (1 - p.values)^(s - 1)/beta(r, s)))
+        return(Target.Func)
+    }
+    
+    ## inital value(MME and non-parametric)
+    mean.p = mean(p.values)
+    var.p = var(p.values)
+    init.r = ((1 - mean.p) * mean.p^2 - mean.p * var.p)/var.p
+    init.s = ((1 - mean.p)^2 * mean.p - (1 - mean.p) * var.p)/var.p
+    init = c(max(0, min(init.r, 0.9)), max(1, init.s))
+    
+    if (restrict) {
+        lambda = max(min(CBUM(p.values, thresh.censor = thresh.p), l.upper), 0.8)
+        r.upper = 0.9
+        s.upper = Inf
+        r.lower = 0
+        s.lower = 1
+    } else {
+        lambda = min(CBUM(p.values, thresh.censor = thresh.p), 0.99)
+        r.upper = 1
+        s.upper = Inf
+        r.lower = 0
+        s.lower = 1
+    }
+    if (unique(tryCatch(print(c(optim(init, fBUM, d = p.values, method = "L-BFGS-B", upper = c(r.upper, s.upper), lower = c(r.lower, 
+        s.lower))$par)), error = function(e) {
+        print("error")
+    }) == "error")) {
+        return(init)
+    } else {
+        pars = optim(init, fBUM, d = p.values, method = "L-BFGS-B", upper = c(r.upper, s.upper), lower = c(r.lower, s.lower))$par
+        LL = sum(log(lambda + (1 - lambda) * p.values^(pars[1] - 1) * (1 - p.values)^(pars[2] - 1)/beta(pars[1], pars[2])))
+        return(c(lambda, pars, LL))
+    }
 }
 
 
 
 # Get dmr when controling true FDR at 0.05 level
-get.dm.regions<-function(p,level=0.05,dmr){
-  p.de<-p[dmr]
-  p.ordered<-p[order(p)]
-  tdr<-rep(0,length(p))
-  for(i in 1:length(p)){
-    tdr[i]=sum(p.de<=p.ordered[i])/sum(p.ordered<=p.ordered[i])
-  }
-  id<-which(tdr>=1-level)
-  if(length(id)==0){
-    return(print("0 DMR found"))
-  }else num.de<-max(id)
-  claim.dmr<-order(p)[1:num.de]
-  return(claim.dmr)
+get.dm.regions <- function(p, level = 0.05, dmr) {
+    p.de <- p[dmr]
+    p.ordered <- p[order(p)]
+    tdr <- rep(0, length(p))
+    for (i in 1:length(p)) {
+        tdr[i] = sum(p.de <= p.ordered[i])/sum(p.ordered <= p.ordered[i])
+    }
+    id <- which(tdr >= 1 - level)
+    if (length(id) == 0) {
+        return(print("0 DMR found"))
+    } else num.de <- max(id)
+    claim.dmr <- order(p)[1:num.de]
+    return(claim.dmr)
 }
 
 
